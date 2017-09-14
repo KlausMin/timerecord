f7e4af3f3985e413dc29794c784f10efb0ff106f
Kynyth <skyfpt1@gmail.com>
2017-09-14 17:19:30 +0900

    170914

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    stamp(){
+      this.str.pushObject(new Date());
+    }
+  },
+  model(){
+    this.str = [];
+    return this.str;
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set("currentTime", new Date());
+  }
 });
